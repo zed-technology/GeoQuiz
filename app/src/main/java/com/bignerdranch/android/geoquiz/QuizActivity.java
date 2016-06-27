1b0d597ae11c45c619c8e910299c162b2fdef90e
Denis Klimenko <suncoin@gmail.com>
2016-06-27 10:29:28 +0300

    add OnClick Listeners 2

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -24,5 +24,12 @@ public class QuizActivity extends AppCompatActivity {
         });
 
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet, but soon!
+            }
+        });
+
     }
 }
